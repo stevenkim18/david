0ea6c2a094cbfbeeb15947deed5b3a7a58384d89
stevenkim18 <11882@naver.com>
2025-08-13 10:05:17 +0900

    Feat: Test3

--- app.py
+++ app.py
@@ -18,5 +18,9 @@ def test1():
 def test2():
     return render_template('test2.html')
 
+@app.route("/test3")
+def test2():
+    return render_template('test2.html')
+
 if __name__ == "__main__":
     app.run(port=8080)
