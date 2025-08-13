c09f432da85724026ed1b0f1e2d5867c6c4ff071
stevenkim18 <11882@naver.com>
2025-08-13 11:34:02 +0900

    test1 함수 코드를 삭제

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ def root():
 def menu():
     return render_template('menu.html')
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-
 @app.route("/test2")
 def test2():
     return render_template('test2.html')
